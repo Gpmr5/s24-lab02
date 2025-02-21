8ccc8f5510aa2a6156d490c9c4aec00821967b83
Gpmr5 <ganaa2z2t@gmail.com>
2025-02-21 10:55:02 +0800

    second commit

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -7,6 +7,7 @@ public class Main {
         Shape rectangle = new Rectangle(2, 3);
         Shape circle = new Circle(5);
 
+        System.out.println();
         Renderer rectangleRenderer = new Renderer(rectangle);
         Renderer circleRenderer = new Renderer(circle);
 
